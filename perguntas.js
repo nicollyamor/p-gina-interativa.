0285622797f8d535f65f148617fef46e648e25f3
nicollyamor <nicolly.reglly@escola.pr.gov.br>
2025-06-04 19:25:20 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,19 +1,23 @@
 criaCartao(
+    'livros'
     'qual seu tipo de livro favorito?',
     ' fantasia!!!'
 )
 
 criaCartao(
+     'livros'
     'Qual o seu livro favorito?',
     'melhor doque nos filmes'
 )
 
 criaCartao(
+     'livros'
     'se pudesse viver um dia em um universo de livro qual seria?',
     'quarta asa'
 )
 
 criaCartao(
+     'livros'
     'com qual personagem voce se identifica no universo dos livros?',
     'liz buxbaum!!'
 )
